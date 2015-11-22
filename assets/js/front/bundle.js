18fff42774aa50b8d9b9c2f7862d5db62a7f27ad
wi2 <contact@wi2prod.fr>
2015-11-22 21:42:53 +0100

    add calendar to panel

--- assets/js/front/bundle.js
+++ assets/js/front/bundle.js
@@ -1168,23 +1168,25 @@ module.exports = exports['default'];
 },{"../lib/agenda":12,"./calendar":4,"./date-picker":5,"lodash":"lodash","newforms":"newforms","newforms-bootstrap":"newforms-bootstrap","react":"react"}],7:[function(require,module,exports){
 "use strict";
 
-Object.defineProperty(exports, "__esModule", {
+Object.defineProperty(exports, '__esModule', {
   value: true
 });
 
-var _createClass = (function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; })();
+var _createClass = (function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ('value' in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; })();
 
-var _get = function get(_x, _x2, _x3) { var _again = true; _function: while (_again) { var object = _x, property = _x2, receiver = _x3; _again = false; if (object === null) object = Function.prototype; var desc = Object.getOwnPropertyDescriptor(object, property); if (desc === undefined) { var parent = Object.getPrototypeOf(object); if (parent === null) { return undefined; } else { _x = parent; _x2 = property; _x3 = receiver; _again = true; desc = parent = undefined; continue _function; } } else if ("value" in desc) { return desc.value; } else { var getter = desc.get; if (getter === undefined) { return undefined; } return getter.call(receiver); } } };
+var _get = function get(_x, _x2, _x3) { var _again = true; _function: while (_again) { var object = _x, property = _x2, receiver = _x3; _again = false; if (object === null) object = Function.prototype; var desc = Object.getOwnPropertyDescriptor(object, property); if (desc === undefined) { var parent = Object.getPrototypeOf(object); if (parent === null) { return undefined; } else { _x = parent; _x2 = property; _x3 = receiver; _again = true; desc = parent = undefined; continue _function; } } else if ('value' in desc) { return desc.value; } else { var getter = desc.get; if (getter === undefined) { return undefined; } return getter.call(receiver); } } };
 
-function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { 'default': obj }; }
 
-function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError('Cannot call a class as a function'); } }
 
-function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
+function _inherits(subClass, superClass) { if (typeof superClass !== 'function' && superClass !== null) { throw new TypeError('Super expression must either be null or a function, not ' + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
 
 var _react = require('react');
 
 var _react2 = _interopRequireDefault(_react);
+
+var _datePicker = require('./date-picker');
 
 var _default = (function (_Component) {
   _inherits(_default, _Component);
@@ -1192,20 +1194,29 @@ var _default = (function (_Component) {
   function _default(props) {
     _classCallCheck(this, _default);
 
-    _get(Object.getPrototypeOf(_default.prototype), "constructor", this).call(this, props);
+    _get(Object.getPrototypeOf(_default.prototype), 'constructor', this).call(this, props);
   }
 
   _createClass(_default, [{
-    key: "render",
+    key: '_onSelect',
+    value: function _onSelect() {}
+  }, {
+    key: 'render',
     value: function render() {
       var _this = this;
 
-      return _react2["default"].createElement(
-        "div",
-        { className: "agenda-panel" },
+      return _react2['default'].createElement(
+        'div',
+        { className: 'agenda-panel' },
+        _react2['default'].createElement(_datePicker.DatePicker, { view: 'month',
+          year: this.props.date.getFullYear(),
+          month: this.props.date.getMonth(),
+          day: this.props.date.getDate(),
+          onSelect: this._onSelect.bind(this) }),
+        _react2['default'].createElement('hr', null),
         Object.keys(this.props).map(function (key) {
-          return _react2["default"].createElement(
-            "div",
+          return _react2['default'].createElement(
+            'div',
             { key: key },
             _this.props[key].toString()
           );
@@ -1217,10 +1228,10 @@ var _default = (function (_Component) {
   return _default;
 })(_react.Component);
 
-exports["default"] = _default;
-module.exports = exports["default"];
+exports['default'] = _default;
+module.exports = exports['default'];
 
-},{"react":"react"}],8:[function(require,module,exports){
+},{"./date-picker":5,"react":"react"}],8:[function(require,module,exports){
 (function (global){
 "use strict";
 
@@ -1702,8 +1713,8 @@ var _default = (function () {
           }
 
           //adjust month and year
-          var yearTmp = y,
-              monthTmp = m;
+          var yearTmp = Number(y),
+              monthTmp = Number(m);
           if (tmp < -20) monthTmp -= 1;else if (tmp < 0) monthTmp += 1;
           if (monthTmp < 0) {
             monthTmp = 11;
--- components/front/com/panel.js
+++ components/front/com/panel.js
@@ -1,16 +1,28 @@
 "use strict";
 
 import React, {Component} from 'react';
+import {DatePicker} from './date-picker'
 
 
 export default class extends Component {
   constructor(props) {
     super(props)
   }
+  _onSelect() {
+
+  }
 
   render() {
+
+
     return (
       <div className="agenda-panel">
+        <DatePicker view="month"
+                    year={this.props.date.getFullYear()}
+                    month={this.props.date.getMonth()}
+                    day={this.props.date.getDate()}
+                    onSelect={this._onSelect.bind(this)}  />
+        <hr />
         {Object.keys(this.props).map(key => <div key={key}>{this.props[key].toString()}</div>)}
       </div>
     )
