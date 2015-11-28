ed3624af2c705ef1f6eb9685ce9aba78e59c2fd3
Michael Gaeta <michael.gaeta@af83.com>
2015-11-28 18:09:56 +0100

    policies: events: fix query

--- api/policies/checkEvent.js
+++ api/policies/checkEvent.js
@@ -1,30 +1,36 @@
 module.exports = function(req, res, next) {
-  // console.log(req.allParams())
-  var countMax = 0;
+  var query = {
+    room: req.param('room'),
+    or: [
+      {
+        start: {
+          '<=': new Date(req.param('start'))
+        },
+        end: {
+          '>=': new Date(req.param('start'))
+        }
+      }, {
+        start: {
+          '<=': new Date(req.param('end'))
+        },
+        end: {
+          '>=': new Date(req.param('end'))
+        }
+      }
+    ]
+  };
+
   switch(req.method) {
-    case 'PUT': countMax = 1; break;
+    case 'PUT': _.extend(query, { id: {'!' : req.param('id')} })
+      break;
+    case 'POST': break;
     default: return next(); break;
   }
 
   Event
-    .count({
-      room: req.param('room'),
-      or: [
-        {
-          start: {
-            '>=': new Date(req.param('start')),
-            '<=': new Date(req.param('end'))
-          }
-        }, {
-          end: {
-            '>=': new Date(req.param('start')),
-            '<=': new Date(req.param('end'))
-          }
-        }
-      ]
-    })
+    .count(query)
     .then( count => {
-      if (count > countMax)
+      if (count)
         res.json({ error: "Check another date for this reservation." })
       else
         next()
