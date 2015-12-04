590c6dac5882796c683ea1b0867b1eca13448c03
Michael Gaeta <michael.gaeta@af83.com>
2015-12-04 15:10:22 +0100

    policies, checkEvent: fix and add check authorization

--- api/policies/checkEvent.js
+++ api/policies/checkEvent.js
@@ -1,6 +1,10 @@
 module.exports = function(req, res, next) {
 
-  var dateStart = new Date(req.param('start'))
+  if (req.method === 'GET')
+    return next();
+
+  var currentUserWithRole
+    , dateStart = new Date(req.param('start'))
     , dateEnd = new Date(req.param('end'))
     , query = {
         room: req.param('room'),
@@ -11,26 +15,39 @@ module.exports = function(req, res, next) {
         ]
       };
 
-  switch(req.method) {
-    case 'PUT':
-      query.id = {'!' : req.param('id')}
-      query.member = req.session.passport.user;//only user was created this event can update this
-      req.body.member = req.session.passport.user;
-      break;
-    case 'POST':
-      req.body.member = req.session.passport.user;
-      break;
-    default: next(); return; break;
-  }
+  User
+    .findOneById(req.session.passport.user)
+    .populate('role')
+    .then( user => {//check user role
+      if (!user)
+        return res.json({ error: "You are not a member" });
+      currentUserWithRole = user
 
-  Event
-    .count(query)
-    .then( count => {
-      if (count)
-        res.json({ error: "Check another date for this reservation." })
-      else
-        next()
+      if (req.method === 'POST') {
+        req.body.member = req.session.passport.user;
+        Event.count(query).then( count => {//before create, check if an event already exist
+          if (count)
+            return res.json({ error: "Check another date for this reservation." });
+          else
+            return next();
+        })
+      } else
+        Event
+          .findOneById(req.param('id'))
+          .then( evt => {//check if you can update or delete this event
+            if ( evt && (evt.member == req.session.passport.user || currentUserWithRole.role.name == 'admin') )
+              return Event.count( _.extend({'!' : req.param('id')}, query) )
+            else
+              return { error: "You are not authorize." };
+          })
+          .then( count => {//check if an event already exist
+            if (count.error)
+              return res.json(count);
+            else if (count > 0)
+              return res.json({ error: "Check another date for this reservation." });
+            else
+              return next()
+          })
     })
-
 
 };
