0c5b75cc896fb182688caca04702607ca39f6759
wi2 <contact@wi2prod.fr>
2016-02-28 22:32:43 +0100

    replace body by root

--- components/front/app.js
+++ components/front/app.js
@@ -1,7 +1,7 @@
 import {render} from 'react-dom';
 import {GetRouter} from './index'
 
-render(GetRouter(), document.getElementById('body'));
+render(GetRouter(), document.getElementById('root'));
 delete global.__ReactInitState__;
 
 // // decomment for testing perf
