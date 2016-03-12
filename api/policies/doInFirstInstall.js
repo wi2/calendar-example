8b91d96dab66c0c314987ffc113350496c66d997
wi2 <contact@wi2prod.fr>
2016-03-12 02:27:05 +0100

    fix create Admin in firstInstall, TODO: have to change that

--- api/policies/doInFirstInstall.js
+++ api/policies/doInFirstInstall.js
@@ -1,15 +1,34 @@
+var crypto = require('crypto')
+
 module.exports = function(req, res, next) {
   User.count().exec((err, count) => {
-    if (count === 0) {
+    // if (count === 0) {
+      console.log("Create Role Admin")
       Role.create({name: "admin"}).exec((err, role) => {
-        User.create({
-          username: "admin",
-          email: "michael.gaeta@af83.com",
-          password: "passpass",
+        const profile = {
+          username: "administrator",
+          email: "admin@example.com",
+          password: "password",
           role: role.id
+        }
+        console.log(role, 'was created')
+        console.log("Create User Admin")
+        User.create(profile, (err, user) => {
+          if (err) return next(err)
+          var token = crypto.randomBytes(48).toString('base64')
+          Passport.create({
+            protocol    : 'local',
+            password    : profile.password,
+            user        : user.id,
+            accessToken : token
+          }, (err, passport) => {
+            console.log("Admin registering is finish")
+
+          })
         })
       })
-    }
+    // }
   })
-  next();
+  next()
+
 };
